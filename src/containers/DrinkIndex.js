8ed3e2bae3c44b4d7ae2fd141604e530520cc251
Omar <omar.branez@gmail.com>
2022-01-01 18:22:02 -0500

    arrange drinks index in a grid

--- src/containers/DrinkIndex.js
+++ src/containers/DrinkIndex.js
@@ -2,6 +2,17 @@ import React, { useEffect, useRef } from 'react'
 import { connect, useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { getDrinks } from '.././actions/actionsDrinks'
+import { experimentalStyled as styled } from '@mui/material/styles'
+import Box from '@mui/material/Box'
+import Paper from '@mui/material/Paper'
+import Grid from '@mui/material/Grid'
+
+const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
 
 const DrinkIndex = (props) => {
 
@@ -13,11 +24,24 @@ const DrinkIndex = (props) => {
 
     return(
         <div>
-            {props.drinks.map(drink => 
+            {/* {props.drinks.map(drink => 
             <Link to={`/drinks/${drink.id}`}>
                 <span>{drink.name}</span>
                 <br/>
-            </Link>)}
+            </Link>)} */}
+            <h1>List of Drinks</h1>
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                    {props.drinks.map((drink, index) => (
+                    <Grid item xs={2} sm={4} md={4} key={index}>
+                        <Link to={`/drinks/${drink.id}`}>
+                        <Item>{drink.name}</Item>
+                        <img src={drink.photo_url} height='200px'></img>
+                        </Link>
+                    </Grid>
+                    ))}
+                </Grid>
+            </Box>
         </div>
     )
 }
