ecabc2e91ca203d5fff7e3b4a2fe27a0d9e9c6e9
Omar <omar.branez@gmail.com>
2022-01-07 03:46:22 -0500

    check off ingredients in a recipe if the user owns them

--- src/components/drinkModal.js
+++ src/components/drinkModal.js
@@ -4,17 +4,28 @@ import { Link } from 'react-router-dom'
 import { setSelectedDrink, unsetSelectedDrink } from '../actions/actionsDrinks'
 import CardContent from '@mui/material/CardContent'
 import Typography from '@mui/material/Typography'
+import CheckIcon from '@mui/icons-material/Check'
 
 const DrinkModal = ({drinkId}) => {
     
     const dispatch = useDispatch()
 
     const drink = useSelector(state => state.drinks.selectedDrink)
+    const userProducts = useSelector(state => state.user.userProducts)
 
     useEffect(()=>{
         dispatch(setSelectedDrink(drinkId))
         return unsetSelectedDrink
     }, [dispatch])
+
+    const ownedProduct = (product) => {
+        console.log(product)
+        if (userProducts.some(prod => prod.id == product.id)){
+            return true
+        } else {
+            return false
+        }
+    }
 
     return (
         <CardContent>
@@ -23,7 +34,8 @@ const DrinkModal = ({drinkId}) => {
             </Typography>
             <Typography> 
                 Ingredients:
-                {drink.ingredients.map(ing => <ul>{ing.quantity} <Link to={`/products/${ing.product.id}`}>{ing.product.name}</Link></ul>)}
+                {drink.ingredients.map(ing => 
+                <ul>{ownedProduct(ing.product) && <CheckIcon/>}{ing.quantity} <Link to={`/products/${ing.product.id}`}> {ing.product.name}</Link></ul>)}
             </Typography>
             <Typography paragraph>
                 {drink.instructions}
